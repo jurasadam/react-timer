6491f32dbbee85167795d5ecd8cac3b8347d9204
Adam Juras <hi@adamjuras.com>
2017-03-29 20:07:33 +0200

    Initial commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,33 @@
+var React = require('react')
+var {Link, IndexLink} = require('react-router')
+
+var Nav = () => {
+  return (
+    <div>
+      <div className='top-bar'>
+        <div className='top-bar-left'>
+          <ul className='menu'>
+            <li className='menu-text'>
+              React Weather App
+            </li>
+            <li>
+              <IndexLink to='/' activeClassName='active'> Timer </IndexLink>
+            </li>
+            <li>
+              <Link to='/' activeClassName='active'> Countdown </Link>
+            </li>
+          </ul>
+        </div>
+        <div className='top-bar-right'>
+          <ul className='menu'>
+            <li className='menu-text'>
+              Created by <a href='http://adamjuras.com' target='_blank'>Adam Juras</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+module.exports = Nav
